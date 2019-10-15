805a40f3ada3c0ca736dc14451ae4e2ad9787b23
senem umac <senemumac@gmail.com>
2019-10-15 13:05:18 +0300

    Migration from gitlab

---
+++ src/main/java/com/bola/chat/model/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.bola.chat.model;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
